20b25faaf06a9a750691cdc1699410989f1d0e65
tiwavaldese <tiwaval7@gmail.com>
2024-01-26 18:59:21 +0100

    updating the files

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+let selectedRow = null
+
 function onFormSubmit(){
-let formData = readFormData()
-insertNewRecord(formData)
+let formData = readFormData();
+if(selectedRow == null)
+   insertNewRecord(formData)
+else
+   updateRecord(formData)
+resetForm()
 }
 
 function readFormData(){
@@ -41,7 +47,54 @@ function insertNewRecord(data){
     cell9 = newRow.insertCell(8);
     cell9.innerHTML = data.workEnd;
     cell10 = newRow.insertCell(9);
-    cell10.innerHTML = `<a>Edit</a>
-                        <a>Delete</a>`
+    cell10.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                        <a onClick="onDelete(this)">Delete</a>`
 }
 
+function resetForm(){
+    document.getElementById("date").value= "";
+    document.getElementById("workStart").value = "";
+    document.getElementById("fullName").value = "";
+    document.getElementById("empCode").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("salary").value = "";
+    document.getElementById("attendance").value = "";
+    document.getElementById("city").value = "";
+    document.getElementById("workEnd").value = "";
+    selectedRow = null
+}
+
+function onEdit(td){
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("date").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("workStart").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("fullName").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("empCode").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[4].innerHTML;
+    document.getElementById("salary").value = selectedRow.cells[5].innerHTML;
+    document.getElementById("attendance").value = selectedRow.cells[6].innerHTML;
+    document.getElementById("city").value = selectedRow.cells[7].innerHTML;
+    document.getElementById("workEnd").value = selectedRow.cells[8].innerHTML;
+}
+
+function updateRecord(formData){
+     selectedRow.cells[0].innerHTML = formData.date;
+     selectedRow.cells[1].innerHTML = formData.workStart;
+     selectedRow.cells[2].innerHTML = formData.fullName;
+     selectedRow.cells[3].innerHTML = formData.empCode;
+     selectedRow.cells[4].innerHTML = formData.email;
+     selectedRow.cells[5].innerHTML = formData.salary;
+     selectedRow.cells[6].innerHTML = formData.attendance;
+     selectedRow.cells[7].innerHTML = formData.city;
+     selectedRow.cells[8].innerHTML = formData.workEnd;
+
+}
+
+function onDelete(td){
+    if(confirm("Are you sure to delete this record!?")){
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+   
+}
